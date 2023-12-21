576646f3c32344dd5c04b110f0607ac9008176e7
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-12-21 12:48:11 +0200

    Add axios, react-hook-form, tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-
-function App() {
+export default function App() {
   return (
     <div>
       <p>Welcome to Luna Edge technical interview</p>
     </div>
-  )
+  );
 }
-
-export default App
